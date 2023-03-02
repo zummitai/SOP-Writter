ff1d71c3bae44b486f0cdfa7d8bce6e71e8dc776
Brain Philemon <philbrainy@gmail.com>
2023-03-02 18:22:26 +0100

    cors origin to accept all requests

--- SOP_Writing/app.py
+++ SOP_Writing/app.py
@@ -11,15 +11,9 @@ app = FastAPI( title="SOP Letter Writting",
     version="0.0.1",)
 
 # Add CORS middleware
-origins = [
-    "http://localhost",
-    "http://localhost:8000",
-    "http://localhost:3000",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
